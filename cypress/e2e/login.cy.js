9b84425711555a1554d1507eed479dba8243b994
Alana <alanareis10@gmail.com>
2025-09-30 22:46:24 -0300

    Implementando comandos customizaveis para Login, Transferencias e commons

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -5,14 +5,7 @@ describe('Login', () => {
   });
   it('Login com dados válidos deve permitir entrada no sistema', () => {
     //Act
-    cy.fixture('credenciais').then(credenciais => {
-      cy.get('#username').click().type(credenciais.valida.usuario)
-      cy.get('#senha').click().type(credenciais.valida.senha)
-    })
-
-    cy.screenshot('apos-preencher-usuario-e-senha')
-    cy.contains('button', 'Entrar').click()
-    cy.screenshot('apos-clicar-em-entrar')  
+    cy.fazerLoginComCredenciaisValidas() 
 
     //Assert
     cy.contains('h4', 'Realizar Transferência').should('be.visible')  
@@ -21,14 +14,10 @@ describe('Login', () => {
   it('Login com dados inválidos deve apresentar mensagem de erro', () => {
 
     //Act
-    cy.fixture('credenciais').then((credenciais) => {
-      cy.get('#username').click().type(credenciais.invalida.usuario)
-      cy.get('#senha').click().type(credenciais.invalida.senha)
-    })
-    cy.contains('button', 'Entrar').click()
+    cy.fazerLoginComCredenciaisIvalidas()
 
     //Assert
-    cy.get('.toast').should('have.text', 'Erro no login. Tente novamente.')
+    cy.verificarMensagemNoToast('Erro no login. Tente novamente.')
   })
 
 })
